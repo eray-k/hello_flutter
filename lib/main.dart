69c14eaef28e7d87c4fc9d006d90dcc79b142455
300Spartian <47210403+eray-k@users.noreply.github.com>
2023-07-12 19:05:14 +0300

    firebase init, login/register views

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hello_world/firebase_options.dart';
+import 'package:hello_world/views/login_view.dart';
+import 'package:hello_world/views/register_view.dart';
 
 FirebaseAuth auth = FirebaseAuth.instance;
 
+/*
+USERS
+--------
+abc.com
+*/
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
@@ -14,52 +25,48 @@ void main() {
   ));
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
-        backgroundColor: Colors.blue,
+        title: const Text('Home Page'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _email,
-          ),
-          TextField(
-            controller: _password,
-          ),
-          TextButton(
-            onPressed: () async {},
-            child: const Text('Register'),
-          )
-        ],
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              print(user);
+              if (user?.emailVerified ?? false) {
+                print('Verified User');
+              } else {
+                print('You need to verify your email first');
+              }
+              return const Text('Done');
+            default:
+              return const Text('Loading... ');
+          }
+        },
       ),
     );
+  }
+}
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
